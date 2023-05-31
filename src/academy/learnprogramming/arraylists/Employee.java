219f88013d46f6b82491c94355a6bff86d23d48d
Tootin0730 <yisan1004@gmail.com>
2023-05-31 18:37:09 +0900

    Vectors

--- src/academy/learnprogramming/arraylists/Employee.java
+++ src/academy/learnprogramming/arraylists/Employee.java
@@ -2,37 +2,13 @@ package academy.learnprogramming.arraylists;
 
 public class Employee {
 
-    private String firstName;
-    private String lastName;
-    private int id;
+    private final String firstName;
+    private final String lastName;
+    private final int id;
 
     public Employee(String firstName, String lastName, int id) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
         this.id = id;
     }
 
@@ -64,4 +40,5 @@ public class Employee {
                 ", id=" + id +
                 '}';
     }
+
 }
--- src/academy/learnprogramming/arraylists/Main.java
+++ src/academy/learnprogramming/arraylists/Main.java
@@ -1,27 +1,17 @@
 package academy.learnprogramming.arraylists;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        List<Employee> employeeList = new ArrayList<>();
+        List<Employee> employeeList = new Vector<>();
         employeeList.add(new Employee("Jane", "Jones", 123));
         employeeList.add(new Employee("John", "Doe", 4567));
         employeeList.add(new Employee("Mary", "Smith", 22));
         employeeList.add(new Employee("Mike", "Wilson", 3245));
-
-        employeeList.set(1, new Employee("John", "Adams", 4568));
-
-        employeeList.add(3, new Employee("John", "Doe", 4567));
-
-        System.out.println(employeeList.contains(new Employee("Mary", "Smith", 22)));
-        System.out.println(employeeList.indexOf(new Employee("John", "Doe", 4567)));
-
-        employeeList.remove(2);
-        employeeList.forEach(employee -> System.out.println(employee));
 
     }
 }
